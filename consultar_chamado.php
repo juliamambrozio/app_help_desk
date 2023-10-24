17464b4149d239e83b9373f1dd3c9a0e15ba89a3
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-10-24 15:16:39 -0300

    exibindo chamados

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,6 +1,27 @@
 <?php
 require_once("validador_acesso.php") //incorporando a session
 ?>
+
+<?php
+
+//chamados
+
+$chamados = array();
+//abrir arquivo
+$arquivo = fopen('arquivo.txt', 'r'); //r = exibir
+
+while(!feof($arquivo)){ //feof = percorre um arquivo até seu final, ! para passar de false para true indicando se é o final ou não
+  //linhas
+  $registro = fgets($arquivo); //recupera o que estiver na linha
+
+  $chamados[] = $registro;
+
+}
+//fechar o arquivo aberto
+fclose($arquivo);
+
+
+?>
 <html>
   <head>
     <meta charset="utf-8" />
@@ -41,24 +62,25 @@ require_once("validador_acesso.php") //incorporando a session
             </div>
             
             <div class="card-body">
-              
+
+            <?php foreach($chamados as $chamado){ ?>
+              <?php
+              $chamado_dados = explode('#', $chamado);
+
+              if(count($chamado_dados) < 3){ //quantidade de índices
+                continue;
+              }
+
+            ?>
               <div class="card mb-3 bg-light">
                 <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+                  <h5 class="card-title"> <?= $chamado_dados[0]?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1]?></h6>
+                  <p class="card-text"> <?= $chamado_dados[2]?></p>
 
                 </div>
               </div>
-
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              <?php } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
